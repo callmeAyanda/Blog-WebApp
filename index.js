06d2d223ee478b5cb62ef4a70fa048d111e11e1e
callmeAyanda <a22.makhubu@gmail.com>
2025-07-14 21:32:32 +0200

    port link update

--- index.js
+++ index.js
@@ -54,5 +54,5 @@ app.post("/delete/:id", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port http://localhost:${port}`);
 });
